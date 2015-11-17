b9f504a7bec7e2ddc884355dc5555919078c6608
Paolo Cavanna <paolo_cavanna@hotmail.com>
2015-11-17 16:24:11 +0100

    Wrapped docCookies into an IIFE

--- js/utils.js
+++ js/utils.js
@@ -676,69 +676,77 @@ MYAPP._EVENTS_ = {
 * @class docCookies
 * @see https://developer.mozilla.org/en-US/docs/Web/API/document.cookie
 */
-MYAPP.docCookies = {
+(function(app, d){
 
-	getItem: function(sKey) {
+	"use strict";
 
-		return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
+	var cookie = d.cookie;
 
-	},
+	app.docCookies = {
 
-	setItem: function(sKey, sValue, vEnd, sPath, sDomain, bSecure) {
+		getItem: function(sKey) {
 
-		if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) {
+			return decodeURIComponent(cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
 
-			return false;
+		},
 
-		}
+		setItem: function(sKey, sValue, vEnd, sPath, sDomain, bSecure) {
 
-		var sExpires = "";
+			if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) {
 
-		if (vEnd) {
+				return false;
 
-			switch (vEnd.constructor) {
-
-				case Number:
-					sExpires = vEnd === Infinity ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT" : "; max-age=" + vEnd;
-					break;
-				case String:
-					sExpires = "; expires=" + vEnd;
-					break;
-				case Date:
-					sExpires = "; expires=" + vEnd.toUTCString();
-					break;
 			}
 
+			var sExpires = "";
+
+			if (vEnd) {
+
+				switch (vEnd.constructor) {
+
+					case Number:
+						sExpires = vEnd === Infinity ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT" : "; max-age=" + vEnd;
+						break;
+					case String:
+						sExpires = "; expires=" + vEnd;
+						break;
+					case Date:
+						sExpires = "; expires=" + vEnd.toUTCString();
+						break;
+				}
+
+			}
+
+			cookie = encodeURIComponent(sKey) + "=" + encodeURIComponent(sValue) + sExpires + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "") + (bSecure ? "; secure" : "");
+
+			return true;
+
+		},
+
+		removeItem: function(sKey, sPath, sDomain) {
+
+			if (!sKey || !this.hasItem(sKey)) {
+
+				return false;
+
+			}
+
+
+			cookie = encodeURIComponent(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "");
+
+			return true;
+
+		},
+
+		hasItem: function(sKey) {
+
+			return (new RegExp("(?:^|;\\s*)" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(cookie);
+
 		}
 
-		document.cookie = encodeURIComponent(sKey) + "=" + encodeURIComponent(sValue) + sExpires + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "") + (bSecure ? "; secure" : "");
+	};
 
-		return true;
-
-	},
-
-	removeItem: function(sKey, sPath, sDomain) {
-
-		if (!sKey || !this.hasItem(sKey)) {
-
-			return false;
-
-		}
-
-
-		document.cookie = encodeURIComponent(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "");
-
-		return true;
-
-	},
-
-	hasItem: function(sKey) {
-
-		return (new RegExp("(?:^|;\\s*)" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
-
-	}
-
-};
+})(MYAPP, window.document);
 
 // make it safe to use console.log always
 (function(b){function c(){}for(var d="assert,clear,count,debug,dir,dirxml,error,exception,firebug,group,groupCollapsed,groupEnd,info,log,memoryProfile,memoryProfileEnd,profile,profileEnd,table,time,timeEnd,timeStamp,trace,warn".split(","),a;a=d.pop();){b[a]=b[a]||c}})((function(){try
